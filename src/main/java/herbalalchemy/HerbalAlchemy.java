6ec366c0b390107ffc11f36e96282ecdb8e28ff0
bl4ckscor3 <donalddani9612@gmx.de>
2019-05-28 22:42:41 +0200

    add base files

---
+++ src/main/java/herbalalchemy/HerbalAlchemy.java
@@ -0,0 +1,12 @@
+package herbalalchemy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=HerbalAlchemy.MODID, name=HerbalAlchemy.NAME, version=HerbalAlchemy.VERSION)
+public class HerbalAlchemy
+{
+	public static final String MODID = "herbalalchemy";
+	public static final String PREFIX = ":" + MODID;
+	public static final String NAME = "Herbal Alchemy";
+	public static final String VERSION = "0.0.1";
+}
